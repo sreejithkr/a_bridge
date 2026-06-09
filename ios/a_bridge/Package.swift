12b1a6a3cd8763f9e3182b0a4f0fea5d970d0bb0
Giacomo Policicchio <giacomo.Policicchio@tui.com>
2026-06-09 18:08:15 +0200

    Adding SwiftPm compatibility

---
+++ ios/a_bridge/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "a_bridge",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "a-bridge", targets: ["a_bridge"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "a_bridge",
+            dependencies: [],
+            resources: [
+                // If your plugin requires a privacy manifest, for example if it
+                // collects user data, update the PrivacyInfo.xcprivacy file to
+                // describe your plugin's privacy impact, and then uncomment this
+                // line. For more information, see:
+                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
+                // .process("PrivacyInfo.xcprivacy"),
+            ]
+        )
+    ]
+)
